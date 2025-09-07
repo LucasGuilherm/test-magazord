cd8ae7b2693ae72dcc7e194e2a6a562b9d6881ac
Lucas Guilherme <lucassade110@gmail.com>
2025-09-06 23:31:22 -0300

    feat: criado input select dropdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,53 @@
+"use client";
+
+import ButtonSelect from "@/components/ButtonSelect";
+import TabNavigation from "@/components/TabNavigation";
 import UserProfile from "@/components/UserProfile";
+import {
+  BookmarkIcon,
+  BookmarkSquareIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  StarIcon,
+} from "@heroicons/react/24/outline";
+import { FC, useState } from "react";
 
 export default function Home() {
+  const [tabSelected, setTabSelected] = useState(0);
+
   return (
-    <div className="flex-1 max-w-5xl flex-col flex gap-16 md:flex-row items-center md:items-start">
+    <div className="flex max-w-5xl flex-1 flex-col items-center gap-16 md:flex-row md:items-start">
       <UserProfile />
 
-      <div className="flex-1 bg-amber-400">teste</div>
+      <div className="flex flex-1 flex-col gap-12">
+        <TabNavigation selected={tabSelected} setSelected={setTabSelected} />
+
+        <div className="flex justify-between gap-16">
+          <div className="flex w-full items-center border-b border-gray-300">
+            <MagnifyingGlassIcon className="size-6" />
+            <input
+              type="text"
+              placeholder="Search Here"
+              className="px-4 py-2 focus:outline-none"
+            />
+          </div>
+
+          <div className="flex gap-4">
+            <ButtonSelect
+              onChange={(values) => console.log(values)}
+              options={["Lucas", "Bruna"]}
+            >
+              Type
+            </ButtonSelect>
+            <ButtonSelect
+              onChange={(values) => console.log(values)}
+              options={["Lucas", "Bruna"]}
+            >
+              Language
+            </ButtonSelect>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ components/ButtonSelect.tsx
@@ -0,0 +1,71 @@
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import React, { useState } from "react";
+
+type ButtonSelectProps = {
+  children: React.ReactNode;
+  options: string[];
+  onChange: (value: string[]) => void;
+};
+
+const ButtonSelect = ({ children, options, onChange }: ButtonSelectProps) => {
+  const [open, setOpen] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+
+  const handleChange = (option: string) => {
+    let novaSelecao = [...selectedOptions];
+    if (novaSelecao.includes(option)) {
+      novaSelecao = novaSelecao.filter((opt) => opt !== option);
+    } else {
+      novaSelecao.push(option);
+    }
+
+    setSelectedOptions(novaSelecao);
+    onChange(novaSelecao);
+  };
+
+  return (
+    <div className="relative">
+      <button
+        onClick={() => setOpen(!open)}
+        className="flex cursor-pointer items-center gap-2 rounded-full bg-gradient-to-r from-[#0056A6] to-[#0587FF] px-2 py-1 pr-4 text-white"
+      >
+        <ChevronDownIcon
+          className={`size-5 transition-transform ${open ? "rotate-180" : ""}`}
+        />
+        {children}
+      </button>
+
+      {open && (
+        <div
+          onClick={() => setOpen(false)}
+          className="fixed inset-0 z-10 bg-black/40"
+        ></div>
+      )}
+
+      {open && (
+        <ul className="absolute right-0 z-20 mt-2 flex w-48 flex-col gap-2 overflow-hidden rounded-lg bg-[#f2f8fe] p-2 shadow-lg ring-1 ring-black/5">
+          {options.map((option, index) => {
+            const selected = selectedOptions.includes(option);
+            return (
+              <li
+                key={index}
+                className={`${selected ? "text-[#0587FF]" : ""} flex cursor-pointer items-center gap-4 px-4 py-2 hover:bg-[#dbdfe5]`}
+                onClick={() => handleChange(option)}
+              >
+                <input
+                  checked={selected}
+                  type="checkbox"
+                  className="size-4"
+                  onChange={() => handleChange(option)}
+                />
+                <span>{option}</span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ButtonSelect;
